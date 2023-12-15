090176e653fb49c9e8ba424eac204200dd4829e3
bob <robert.matthew.brooks@gmail.com>
2023-12-15 00:06:46 +0000

    all null value for recipe image

--- server/__tests__/recipes.test.js
+++ server/__tests__/recipes.test.js
@@ -1,8 +1,11 @@
 const supertest = require('supertest');
+const matchers = require('jest-extended');
 const server = require('../server');
 const pool = require('../db/pool');
 const seed = require('../db/seed');
 const data = require('../db/data/test');
+
+expect.extend(matchers);
 
 beforeEach(async () => {
   await seed(data);
@@ -23,7 +26,7 @@ describe('GET /recipes/:recipe_id', () => {
       name: expect.any(String),
       slug: expect.any(String),
       author: expect.any(String),
-      img_url: expect.any(String),
+      img_url: expect.toBeOneOf([expect.any(String), null]),
       ingredients: expect.any(Array),
       steps: expect.any(Array),
       is_vegetarian: expect.any(Boolean),
@@ -65,7 +68,7 @@ describe('GET /recipes', () => {
         name: expect.any(String),
         slug: expect.any(String),
         author: expect.any(String),
-        img_url: expect.any(String),
+        img_url: expect.toBeOneOf([expect.any(String), null]),
         likes: expect.any(Number),
       });
     }
