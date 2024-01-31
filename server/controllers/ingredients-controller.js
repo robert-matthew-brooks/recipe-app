224f643683c7811ffc37483b9250aedb5f735932
bob <robert.matthew.brooks@gmail.com>
2024-01-31 01:08:39 +0000

    add endpoint to provide shopping list data

--- server/controllers/ingredients-controller.js
+++ server/controllers/ingredients-controller.js
@@ -1,12 +1,23 @@
 const ingredientsModel = require('../models/ingredients-model');
 
-async function getIngredients(req, res, next) {
+async function getAll(req, res, next) {
   try {
-    const { ingredients } = await ingredientsModel.getIngredients();
+    const { ingredients } = await ingredientsModel.getAll();
     res.send({ ingredients });
   } catch (err) {
     next(err);
   }
 }
 
-module.exports = { getIngredients };
+async function getShoppingList(req, res, next) {
+  const token = req.headers?.authorization?.split(' ')[1];
+
+  try {
+    const { ingredients } = await ingredientsModel.getShoppingList(token);
+    res.send({ ingredients });
+  } catch (err) {
+    next(err);
+  }
+}
+
+module.exports = { getAll, getShoppingList };
--- server/models/ingredients-model.js
+++ server/models/ingredients-model.js
@@ -1,6 +1,7 @@
 const pool = require('../db/pool');
+const { verifyToken } = require('../util/token');
 
-async function getIngredients() {
+async function getAll() {
   const { rows } = await pool.query(
     `
       SELECT * FROM ingredients
@@ -11,4 +12,30 @@ async function getIngredients() {
   return { ingredients: rows };
 }
 
-module.exports = { getIngredients };
+async function getShoppingList(token) {
+  const userId = verifyToken(token).id;
+
+  const { rows } = await pool.query(
+    `
+      SELECT
+        i.name,
+        ri.amount,
+        i.units
+      FROM (
+        SELECT r.id from recipes r
+        INNER JOIN todos t
+        ON r.id = t.recipe_id
+        WHERE t.user_id = ${userId}
+      ) r
+      INNER JOIN recipes_ingredients ri
+      ON r.id = ri.recipe_id
+      INNER JOIN ingredients i
+      ON i.id = ri.ingredient_id
+      ORDER BY i.name ASC;
+    `
+  );
+
+  return { ingredients: rows };
+}
+
+module.exports = { getAll, getShoppingList };
