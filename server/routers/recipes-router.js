855ead63914a622182144706b11c3fff8c29169e
bob <robert.matthew.brooks@gmail.com>
2024-02-09 16:00:11 +0000

    add create recipe endpoint

--- server/routers/recipes-router.js
+++ server/routers/recipes-router.js
@@ -6,6 +6,7 @@ const recipesRouter = express.Router();
 recipesRouter.get('/:recipe_slug', recipesController.getOne);
 recipesRouter.get('/', recipesController.getMany);
 recipesRouter.patch('/:recipe_slug', recipesController.patchRecipe);
+recipesRouter.post('/', recipesController.createRecipe);
 recipesRouter.delete('/:recipe_slug', recipesController.deleteRecipe);
 
 module.exports = recipesRouter;
